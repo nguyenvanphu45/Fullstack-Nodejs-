441a40935c65c79221e9a16e41e9ec95e4e68134
Phu <vanphu040520022gmail.com>
2022-03-05 23:59:18 +0700

    CRUD with React (write API ADD, EDIT, DELETE user)

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -21,7 +21,7 @@ let handleLogin = async (req, res) => {
 }
 
 let handleGetAllUser = async (req, res) => {
-    let id = req.body.id    //All, id
+    let id = req.query.id    //All, id
 
     if (!id) {
         return res.status(200).json({
@@ -40,7 +40,32 @@ let handleGetAllUser = async (req, res) => {
     })
 }
 
+let handleCreateNewUser = async (req, res) => {
+    let message = await userService.createNewUser(req.body)
+    return res.status(200).json(message)
+}
+
+let handleEditUser = async (req, res) => {
+    let data = req.body
+    let message = await userService.updateUserData(data)
+    return res.status(200).json(message)
+}
+
+let handleDeleteUser = async (req, res) => {
+    if (!req.body.id) {
+        return res.status(200).json({
+            errCode: 1,
+            errMessage: 'Missing required parameter!'
+        })
+    }
+    let message = await userService.deleteUser(req.body.id)
+    return res.status(200).json(message)
+}
+
 module.exports = {
     handleLogin: handleLogin,
-    handleGetAllUser: handleGetAllUser
+    handleGetAllUser: handleGetAllUser,
+    handleCreateNewUser: handleCreateNewUser,
+    handleEditUser: handleEditUser,
+    handleDeleteUser: handleDeleteUser
 }
