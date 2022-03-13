eff05a9812e69f8be16c3ad927bf8290f508de85
Phu <vanphu040520022gmail.com>
2022-03-13 20:23:02 +0700

    Import data from EXCEL and write api get ALlCODE (#52)

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -62,10 +62,24 @@ let handleDeleteUser = async (req, res) => {
     return res.status(200).json(message)
 }
 
+let getAllCode = async (req, res) => {
+    try {
+        let data = await userService.getAllCodeService(req.query.type)
+        return res.status(200).json(data)
+    } catch (e) {
+        console.log('check error', e)
+        return res.status(200).json ({
+            errCode: -1,
+            errMessage: 'Error from server'
+        })
+    }
+}
+
 module.exports = {
     handleLogin: handleLogin,
     handleGetAllUser: handleGetAllUser,
     handleCreateNewUser: handleCreateNewUser,
     handleEditUser: handleEditUser,
-    handleDeleteUser: handleDeleteUser
+    handleDeleteUser: handleDeleteUser,
+    getAllCode: getAllCode
 }
