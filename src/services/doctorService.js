d5e9d41d65248ed32ef536dda6452b387861a902
Phu <vanphu040520022gmail.com>
2022-04-12 22:07:38 +0700

    Load data select medical examination schedule (#76)

--- src/services/doctorService.js
+++ src/services/doctorService.js
@@ -165,17 +165,9 @@ let bulkCreateSchedule = (data) => {
                     raw: true
                 })
 
-                //convert date
-                if (existing && existing.length > 0) {
-                    existing = existing.map(item => {
-                        item.date = new Date(item.date).getTime()
-                        return item
-                    })
-                }
-
                 //compare different
                 let toCreate = _.differenceWith(schedule, existing, (a, b) => {
-                    return a.timeType === b.timeType && a.date === b.date
+                    return a.timeType === b.timeType && +a.date === +b.date
                 })
 
                 // create data
@@ -208,7 +200,15 @@ let getScheduleDoctorByDate = (doctorId, date) => {
                     where: {
                         doctorId: doctorId,
                         date: date
-                    }
+                    },
+                    include: [
+                        { 
+                            model: db.Allcode, as: 'timeTypeData', 
+                            attributes: ['valueEn', 'valueVi']
+                        }
+                    ],
+                    raw: false,
+                    nest: true
                 })
 
                 if (!dataSchedule) dataSchedule = []
